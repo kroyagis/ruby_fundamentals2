2f3e373605cab93adcd5b209f0f47117df15429c
Ji You <kroyagis@gmail.com>
2016-05-31 17:01:55 -0400

    Outputs the converted value, rounded to the second decimal place

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,7 @@
-puts "Enter a temperature in Fahrenheit"
-fahrenheit = gets
-
 def converter(temperature)
-  return (temperature.to_i - 32) * 5/9
+  ((temperature.to_f - 32) * 5/9).round(2)
 end
+
+puts "Enter a temperature in Fahrenheit"
+fahrenheit = gets.chomp
+puts "#{fahrenheit}F is #{converter(fahrenheit)}C"
