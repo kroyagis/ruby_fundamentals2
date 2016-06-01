6269e93084fc856eb061b6939522c882c64d6ca6
Ji You <kroyagis@gmail.com>
2016-06-01 14:38:02 -0400

    Deleted tofu from the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,7 @@
-def show_grocery(item)
-  puts "*#{item}"
+def show_grocery(items)
+  items.each do |item|
+    puts "* #{item}"
+  end
 end
 
 grocery_list = ["apple", "onion", "tofu", "nori"]
@@ -21,3 +23,6 @@ end
 puts grocery_list[1]
 
 show_grocery(grocery_list.sort)
+
+grocery_list.delete("tofu")
+show_grocery(grocery_list)
