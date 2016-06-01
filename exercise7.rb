a3c404bb23a1791bff6b0446f1d8563e1c5f82b9
Ji You <kroyagis@gmail.com>
2016-06-01 14:45:58 -0400

    Created a method that displays the name and number of students for each cohort

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,13 @@
 students = {
-  cohort1: 34
-  cohort2: 42
+  cohort1: 34,
+  cohort2: 42,
   cohort3: 22
 }
+
+def number_of_students(student)
+  student.each do |cohort, number|
+    puts "#{cohort}: #{number} students"
+  end
+end
+
+number_of_students(students)
